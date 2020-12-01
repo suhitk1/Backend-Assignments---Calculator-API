cd4f110340481e0e8b5d1dc6d31fd7e7725d078c
suhitk1 <suhitk2112@gmail.com>
2020-12-01 18:21:05 +0530

    updated answer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,31 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
+app.get('/',function(req,res){
+    res.send("Hello World!");
+});
+app.post('/add',function(req,res){
+    //res.send(req.query);
+    const a = Number(req.query.num1);
+    const b = Number(req.query.num2);
+    if(a==='NaN'|| b==='NaN'){
+        res.send({status: "error",
+        message: "Invalid data types"});
+    }
+    if(a<-1000000|| b<-1000000){
+        res.send({status: "failure",
+        message: "Underflow"});
+    }
+    if(a>1000000|| b>1000000){
+        res.send({status: "failure",
+        message: "Overflow"});
+    }
+    res.send({
+        status: "success",
+        message: "the sum of given two numbers",
+        sum: `${a+b}`
+        });
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
