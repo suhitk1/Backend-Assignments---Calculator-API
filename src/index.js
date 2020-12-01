2429405ed3519bad9f033b68e7a439fff3febacf
suhitk1 <suhitk2112@gmail.com>
2020-12-01 18:45:23 +0530

    updated answer

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ app.get('/',function(req,res){
     res.send("Hello World!");
 });
 app.post('/add',function(req,res){
-    //res.send(req.query);
     const a = Number(req.query.num1);
     const b = Number(req.query.num2);
     if(a==='NaN'|| b==='NaN'){
@@ -23,11 +22,11 @@ app.post('/add',function(req,res){
         message: "Invalid data types"});
     }
     if(a<-1000000|| b<-1000000){
-        res.send({status: "failure",
+        res.send({status: "error",
         message: "Underflow"});
     }
     if(a>1000000|| b>1000000){
-        res.send({status: "failure",
+        res.send({status: "error",
         message: "Overflow"});
     }
     res.send({
@@ -36,7 +35,73 @@ app.post('/add',function(req,res){
         sum: `${a+b}`
         });
 });
-
+app.post('/sub',function(req,res){
+    const a = Number(req.query.num1);
+    const b = Number(req.query.num2);
+    if(a==='NaN'|| b==='NaN'){
+        res.send({status: "error",
+        message: "Invalid data types"});
+    }
+    if(a<-1000000|| b<-1000000){
+        res.send({status: "error",
+        message: "Underflow"});
+    }
+    if(a>1000000|| b>1000000){
+        res.send({status: "error",
+        message: "Overflow"});
+    }
+    res.send({
+        status: "success",
+        message: "the difference of given two numbers",
+        difference: `${a-b}`
+        });
+});
+app.post('/multiply',function(req,res){
+    const a = Number(req.query.num1);
+    const b = Number(req.query.num2);
+    if(a==='NaN'|| b==='NaN'){
+        res.send({status: "error",
+        message: "Invalid data types"});
+    }
+    if(a<-1000000|| b<-1000000){
+        res.send({status: "error",
+        message: "Underflow"});
+    }
+    if(a>1000000|| b>1000000){
+        res.send({status: "failure",
+        message: "Overflow"});
+    }
+    res.send({
+        status: "success",
+        message: "The product of given numbers",
+        result: `${a*b}`
+        });
+});
+app.post('/divide',function(req,res){
+    const a = Number(req.query.num1);
+    const b = Number(req.query.num2);
+    if(a==='NaN'|| b==='NaN'){
+        res.send({status: "error",
+        message: "Invalid data types"});
+    }
+    if(b===0){
+        res.send({status: "error",
+        message: "Cannot divide by zero"});
+    }
+    if(a<-1000000|| b<-1000000){
+        res.send({status: "error",
+        message: "Underflow"});
+    }
+    if(a>1000000|| b>1000000){
+        res.send({status: "error",
+        message: "Overflow"});
+    }
+    res.send({
+        status: "success",
+        message: "the sum of given two numbers",
+        result: `${a/b}`
+        });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
